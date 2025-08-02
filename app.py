228b23b64b93055ea86108f106bf89a481d74f84
Bonam24 <bosene@usiu.ac.ke>
2025-08-02 11:02:07 +0300

    added cors

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from predict import load_all_models, predict_image
 from PIL import Image
 import io
 
 app = FastAPI(title="Screen vs Camera Detector API")
+
+# --- Enable CORS ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins. Replace ["*"] with a list of allowed domains for better security.
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Load models once at startup
 model_info = load_all_models()
